4e0d1db25252acbdff9afdbe8e8517b8820aedd1
Michael Flagg <flagg.michael@gmail.com>
2022-04-08 20:31:07 -0500

    Fancy keymap

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/default/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/default/keymap.c
@@ -17,14 +17,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {
-    MUTE_MIC = SAFE_RANGE
+    MUTE_MIC = SAFE_RANGE,
+	TASK_MGR
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case MUTE_MIC:
         if (record->event.pressed) {
-            // when keycode MUTE_MIC is pressed
             register_code(KC_LCTL);
             register_code(KC_LSFT);
             register_code(KC_M);
@@ -32,6 +32,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             unregister_code(KC_LCTL);
             unregister_code(KC_LSFT);
             unregister_code(KC_M);
+        }
+        break;
+	case TASK_MGR:
+        if (record->event.pressed) {
+            register_code(KC_LCTL);
+            register_code(KC_LSFT);
+            register_code(KC_ESC);
+        } else {
+            unregister_code(KC_LCTL);
+            unregister_code(KC_LSFT);
+            unregister_code(KC_ESC);
         }
         break;
     }
@@ -43,19 +54,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
-  KC_GESC,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,        KC_9,       KC_0,     KC_MINS,     KC_EQL,    KC_BSPC,     KC_DEL,
-   KC_TAB,       KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,        KC_O,       KC_P,     KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_PGUP,
-  KC_CAPS,       KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,        KC_L,    KC_SCLN,     KC_QUOT,     KC_ENT,                KC_PGDN,
-  KC_LSFT,       KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,    KC_COMM,      KC_DOT,    KC_SLSH,       KC_F5,      KC_UP,                 KC_END,
-  KC_LCTL,    KC_LGUI,    KC_LALT,                                         KC_SPC,                            MUTE_MIC,      MO(1),     KC_LEFT,    KC_DOWN,                KC_RGHT),
+  KC_GESC,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,        KC_9,       KC_0,     KC_MINS,     KC_EQL,    KC_BSPC,      KC_INS,
+   KC_TAB,       KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,        KC_O,       KC_P,     KC_LBRC,    KC_RBRC,    KC_BSLS,      KC_DEL,
+  KC_CAPS,       KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,        KC_L,    KC_SCLN,     KC_QUOT,     KC_ENT,                   KC_F5,
+  KC_LSFT,       KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,    KC_COMM,      KC_DOT,    KC_SLSH,     KC_LSFT,      KC_UP,                TASK_MGR,
+  KC_LCTL,      MO(1),    KC_LGUI,                                         KC_SPC,                            MUTE_MIC,    KC_RALT,     KC_LEFT,    KC_DOWN,    KC_RGHT),
   
 // Had an extra blank on 2nd to last, was missing blank for enter
 [1] = LAYOUT(
-   KC_GRV,      KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,       KC_F9,     KC_F10,      KC_F11,     KC_F12,    _______,     KC_INS,
-  _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,     _______,    KC_PSCR,     KC_SCRL,    KC_PAUS,    _______,    _______,
-  _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,     _______,    _______,     _______,    _______,                _______,
-  _______,    RGB_HUI,    RGB_HUD,    RGB_SPD,    RGB_SPI,    KC_MUTE,    KC_VOLU,    KC_VOLD,    KC_MPRV,     KC_MPLY,    KC_MNXT,     _______,    RGB_VAI,                KC_HOME,
-  _______,    _______,    _______,                                          RESET,                             _______,    _______,    RGB_RMOD,    RGB_VAD,                RGB_MOD)
+   KC_GRV,      KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,       KC_F9,     KC_F10,      KC_F11,     KC_F12,    _______,     _______,
+  _______,    _______,    RGB_VAI,    _______,    _______,    _______,    _______,    _______,    _______,     _______,    KC_PSCR,     KC_SCRL,    KC_PAUS,    _______,     _______,
+  _______,    RGB_RMOD,   RGB_VAD,    RGB_MOD,    _______,    _______,    _______,    _______,    _______,     _______,    _______,     _______,    _______,                 _______,
+  _______,    RGB_HUI,    RGB_HUD,    RGB_SPD,    RGB_SPI,    KC_MUTE,    KC_VOLU,    KC_VOLD,    KC_MPRV,     KC_MPLY,    KC_MNXT,     _______,    KC_PGUP,                 _______,
+  _______,    _______,    _______,                                          RESET,                             _______,    _______,     KC_HOME,    KC_PGDN,     KC_END)
 };
   
 /*
