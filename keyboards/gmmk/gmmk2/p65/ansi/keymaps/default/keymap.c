ea7adda96f52f39c0572ca6e046369ebc84222b0
Michael Flagg <flagg.michael@gmail.com>
2022-04-11 20:27:25 -0500

    Space cadet mode and allow ctrl/alt + esc

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/default/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/default/keymap.c
@@ -166,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GESC,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,        KC_9,       KC_0,     KC_MINS,     KC_EQL,    KC_BSPC,      KC_INS,
    KC_TAB,       KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,        KC_O,       KC_P,     KC_LBRC,    KC_RBRC,    KC_BSLS,      KC_DEL,
 CAPS_LOCK,       KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,        KC_L,    KC_SCLN,     KC_QUOT,     KC_ENT,                  F5_KEY,
-  KC_LSFT,       KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,    KC_COMM,      KC_DOT,    KC_SLSH,     KC_LSFT,      KC_UP,                TASK_MGR,
+  KC_LSPO,       KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,    KC_COMM,      KC_DOT,    KC_SLSH,     KC_RSPC,      KC_UP,                TASK_MGR,
   KC_LCTL,    WIN_KEY,      MO(1), /*switch to layer 1 while held*/        KC_SPC,                             KC_RALT,   MUTE_MIC,     KC_LEFT,    KC_DOWN,    KC_RGHT),
   
 [_FN_FOR_QWERTY] = LAYOUT(
@@ -180,7 +180,7 @@ CAPS_LOCK,       KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_
   KC_GESC,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,        KC_9,       KC_0,     KC_LBRC,    KC_RBRC,    KC_BSPC,      KC_INS,
    KC_TAB,    KC_QUOT,    KC_COMM,     KC_DOT,       KC_P,       KC_Y,       KC_F,       KC_G,       KC_C,        KC_R,       KC_L,     KC_SLSH,     KC_EQL,    KC_BSLS,      KC_DEL,
 CAPS_LOCK,       KC_A,       KC_O,       KC_E,       KC_U,       KC_I,       KC_D,       KC_H,       KC_T,        KC_N,       KC_S,     KC_MINS,     KC_ENT,                  F5_KEY,
-  KC_LSFT,    KC_SCLN,       KC_Q,       KC_J,       KC_K,       KC_X,       KC_B,       KC_M,       KC_W,        KC_V,       KC_Z,     KC_LSFT,      KC_UP,                TASK_MGR,
+  KC_LSPO,    KC_SCLN,       KC_Q,       KC_J,       KC_K,       KC_X,       KC_B,       KC_M,       KC_W,        KC_V,       KC_Z,     KC_RSPC,      KC_UP,                TASK_MGR,
   KC_LCTL,    WIN_KEY,      MO(3), /*switch to layer 3 while held*/        KC_SPC,                             KC_RALT,   MUTE_MIC,     KC_LEFT,    KC_DOWN,    KC_RGHT),
 
 [_FN_FOR_DVORAK] = LAYOUT(
