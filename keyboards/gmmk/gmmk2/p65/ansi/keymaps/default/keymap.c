faab2712a449d6250c3cb4590b9d456aa6a2691b
Michael Flagg <flagg.michael@gmail.com>
2022-05-04 22:35:59 -0500

    Rename colemak

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/default/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/default/keymap.c
@@ -16,14 +16,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-#define _COLEMAK_DH        0 // keymap 0 is always the default on boot
-#define _FN_FOR_COLEMAK_DH 1
-#define _QWERTY            2
-#define _FN_FOR_QWERTY     3
-#define _COLEMAK           4 // unused
-#define _FN_FOR_COLEMAK    5 // unused
-#define _DVORAK            6 // unused
-#define _FN_FOR_DVORAK     7 // unused
+#define _COLEMAK_DHk        0 // keymap 0 is always the default on boot
+#define _FN_FOR_COLEMAK_DHk 1
+#define _QWERTY             2
+#define _FN_FOR_QWERTY      3
+#define _COLEMAK            4 // unused
+#define _FN_FOR_COLEMAK     5 // unused
+#define _DVORAK             6 // unused
+#define _FN_FOR_DVORAK      7 // unused
+#define _FN_FOR_DVORAK      7 // unused
+#define _FN_FOR_DVORAK      7 // unused
 
 static uint32_t CAPS_LOCK_FLASH_TIMER = 0;
 static bool IN_GAME_MODE = false;
@@ -45,7 +47,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		// Toggle game mode and switch to QWERTY
         if (record->event.pressed) {
             IN_GAME_MODE = !IN_GAME_MODE;
-			layer_move( IN_GAME_MODE ? _QWERTY :_COLEMAK_DH ); // todo save and toggle back to old layout
+			layer_move( IN_GAME_MODE ? _QWERTY :_COLEMAK_DHk ); // todo save and toggle back to old layout
         }
         break;
 	/*case F5_KEY:
@@ -193,20 +195,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // https://colemakmods.github.io/mod-dh/keyboards.html
 
 // Default layer
-[_COLEMAK_DH] = LAYOUT(
+[_COLEMAK_DHk] = LAYOUT(
   KC_GESC,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,        KC_9,       KC_0,     KC_MINS,     KC_EQL,    KC_BSPC,      KC_INS,
    KC_TAB,       KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,       KC_J,       KC_L,       KC_U,        KC_Y,    KC_SCLN,     KC_LBRC,    KC_RBRC,    KC_BSLS,      KC_DEL,
   KC_BSPC,       KC_A,       KC_R,       KC_S,       KC_T,       KC_G,       KC_K,       KC_N,       KC_E,        KC_I,       KC_O,     KC_QUOT,     KC_ENT,                  KC_END,
   KC_LSPO,       KC_X,       KC_C,       KC_D,       KC_V,       KC_Z,       KC_M,       KC_H,    KC_COMM,      KC_DOT,    KC_SLSH,     KC_RSPC,      KC_UP,            LT(1, KC_F5), /*Fn (layer 1) if held or pressed in combo with another key. F5 if tapped.*/
   KC_LCTL,    WIN_KEY,       KC_LALT,                                      KC_SPC,                            MUTE_MIC,    KC_RCTL,     KC_LEFT,    KC_DOWN,    KC_RGHT),
 
-[_FN_FOR_COLEMAK_DH] = LAYOUT(
+[_FN_FOR_COLEMAK_DHk] = LAYOUT(
    KC_GRV,      KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,       KC_F9,     KC_F10,      KC_F11,     KC_F12,    _______,       TO(2), /*switch to layer 2*/
   _______,    _______,    RGB_VAI,    _______,    _______,    _______,    _______,    _______,    _______,     _______,    KC_PSCR,     _______,    KC_PAUS,    _______,     _______,
   _______,    RGB_RMOD,   RGB_VAD,    RGB_MOD,    _______,    _______,    _______,    _______,    _______,     _______,    _______,     _______,    _______,               GAME_MODE,
   _______,    RGB_HUI,    RGB_HUD,    RGB_SPD,    RGB_SPI,    RGB_SAD,    RGB_SAI,    KC_MUTE,    KC_MPRV,     KC_MPLY,    KC_MNXT,     _______,    KC_PGUP,                 _______,
   _______,    _______,    _______,                                          RESET,                             _______,    _______,     KC_HOME,    KC_PGDN,    KC_END),     
 
+// QWERTY
 [_QWERTY] = LAYOUT(
   KC_GESC,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,        KC_9,       KC_0,     KC_MINS,     KC_EQL,    KC_BSPC,      KC_INS,
    KC_TAB,       KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,        KC_O,       KC_P,     KC_LBRC,    KC_RBRC,    KC_BSLS,      KC_DEL,
